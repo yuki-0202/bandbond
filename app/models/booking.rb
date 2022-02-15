1298ece843880215abab609a3684744a24dd4b2b
yuki-0202 <stoicbazooka@gmail.com>
2022-02-15 17:11:25 +0900

    ActiveHashの導入（ジャンル選択）

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,6 @@
 class Booking < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
 end
