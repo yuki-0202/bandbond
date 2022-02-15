4210f1910d0e02c32f760fde3eadffbb5b0ec86d
yuki-0202 <stoicbazooka@gmail.com>
2022-02-15 21:01:42 +0900

    クリッカブルマップの実装

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,10 @@
 class Booking < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :area
 
-  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :genre_id
+    validates :area_id
+  end
 end
