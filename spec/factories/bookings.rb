46366cae77b1286be6b8c9f06ce9ee0ab9745a52
yuki-0202 <stoicbazooka@gmail.com>
2022-02-17 11:46:07 +0900

    FactoryBotの修正

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :booking do
-    area_id { 2 }
-    genre_id { 2 }
-    # area_id { Area.where.not(id: '1').sample }
-    # genre_id { Genre.where.not(id: 1).sample }
+    area { Area.where.not(id: 1).sample }
+    genre { Genre.where.not(id: 1).sample }
     date_start { Date.today }
     date_end { Faker::Date.forward(days: 365) }
     venue { Faker::Lorem.word }
