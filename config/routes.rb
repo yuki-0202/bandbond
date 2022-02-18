2f5dc8e382cd19df24a884a36c45a2dff7b166f1
yuki-0202 <stoicbazooka@gmail.com>
2022-02-18 17:57:32 +0900

    ブッキング削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "bookings#index"
-  resources :bookings, only: [:new, :create, :show, :edit, :update]
+  resources :bookings, only: [:new, :create, :show, :edit, :update, :destroy]
 end
