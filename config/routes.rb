a0974c51237ec49f0536f5af1489a17c3153a732
yuki-0202 <stoicbazooka@gmail.com>
2022-02-23 11:17:27 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "bookings#index"
   resources :bookings, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :rooms, only: [:new, :create]
 end
