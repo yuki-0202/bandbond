77485e584a9eddb3d9e10397ac3f73ee5304da2e
yuki-0202 <stoicbazooka@gmail.com>
2022-02-23 11:12:00 +0900

    roomsテーブルの作成

---
+++ db/migrate/20220223020910_create_rooms.rb
@@ -0,0 +1,8 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.references :booking, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_081113) do
+ActiveRecord::Schema.define(version: 2022_02_23_020910) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "area_id", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_02_14_081113) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_rooms_on_booking_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -44,4 +51,5 @@ ActiveRecord::Schema.define(version: 2022_02_14_081113) do
   end
 
   add_foreign_key "bookings", "users"
+  add_foreign_key "rooms", "bookings"
 end
