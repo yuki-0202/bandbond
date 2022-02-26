48c80fceec63c8950b12b6130cf289da43f72fe5
yuki-0202 <stoicbazooka@gmail.com>
2022-02-26 16:36:41 +0900

    ルーティングの多重ネストを回避する為の修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,17 +4,17 @@ class RoomsController < ApplicationController
     @room = Room.new(room_params)
     if Room.exists?(booking_id: @room.booking_id, user_id: current_user.id)
       @room_id = Room.find_by(booking_id: params[:booking_id], user_id: current_user.id)
-      redirect_to booking_room_path(@room.booking_id, @room_id.id)
+      redirect_to room_path(@room_id.id)
     else
       @room.save
       @room_id = Room.find_by(booking_id: params[:booking_id], user_id: current_user.id)
-      redirect_to booking_room_path(@room.booking_id, @room_id.id)
+      redirect_to room_path(@room_id.id)
     end
   end
 
   def show
-    @booking = Booking.find(params[:booking_id])
     @room = Room.find_by(id: params[:id])
+    @booking = Booking.find(@room.booking_id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "bookings#index"
   resources :bookings do
-    resources :rooms, only: [:create, :show]
+    resources :rooms, only: [:create]
   end
+  resources :rooms, only: [:show]
 end
