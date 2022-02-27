abb799b5b8f86b9776ce7774bc431e1d4f2173a7
yuki-0202 <stoicbazooka@gmail.com>
2022-02-27 16:05:14 +0900

    重複したコードを修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,12 +4,10 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if Room.exists?(booking_id: @room.booking_id, user_id: current_user.id)
-      @room_id = Room.find_by(booking_id: params[:booking_id], user_id: current_user.id)
-      redirect_to room_path(@room_id.id)
+      move_to_room
     else
       @room.save
-      @room_id = Room.find_by(booking_id: params[:booking_id], user_id: current_user.id)
-      redirect_to room_path(@room_id.id)
+      move_to_room
     end
   end
 
@@ -32,5 +30,10 @@ class RoomsController < ApplicationController
   def set_room_set_booking
     @room = Room.find(params[:id])
     @booking = Booking.find(@room.booking_id)
+  end
+
+  def move_to_room
+    room_transition = Room.find_by(booking_id: params[:booking_id], user_id: current_user.id)
+    redirect_to room_path(room_transition.id)
   end
 end
