8ec96db27faead09d9d09b2fdb571d1c467cd2c4
yuki-0202 <stoicbazooka@gmail.com>
2022-02-27 17:52:57 +0900

    map.jsを条件分岐

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,8 +1,14 @@
+const { end } = require("@popperjs/core");
+
 function map (){
   const prefectures = document.querySelectorAll(".prefecture");
   prefectures.forEach(function(pref) {
     pref.addEventListener('click', function() {
-      document.getElementById("test-prefecture").value = pref.id;
+      if (document.getElementById("test-prefecture") != null) {
+        document.getElementById("test-prefecture").value = pref.id;
+      } else {
+// 追加実装（falseの場合、クリックした地域で絞り込みされたブッキング一覧が表示される）
+      }
     });
   });
 };
