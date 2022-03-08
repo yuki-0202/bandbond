fef65249322a8ec1896468ed4f8cb58040378d86
yuki-0202 <stoicbazooka@gmail.com>
2022-03-08 22:55:43 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,26 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :email,
+      :birthday,
+      :sex_id,
+      :genre_id,
+      :area_id,
+      :part_id,
+      :introduction
+    )
+  end
 end
