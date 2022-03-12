40b9fd7d7d14ea30fc069fea2c15145e37dfc579
yuki-0202 <stoicbazooka@gmail.com>
2022-03-12 16:06:50 +0900

    投稿履歴一覧の表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
   before_action :set_search
+  before_action :set_my_bookings
 
   private
 
@@ -18,5 +19,9 @@ class ApplicationController < ActionController::Base
   def set_search
     @q = Booking.ransack(params[:q])
     @search_bookings = @q.result
+  end
+
+  def set_my_bookings
+    @my_bookings = Booking.where(user_id: current_user.id).order('updated_at DESC') if user_signed_in?
   end
 end
