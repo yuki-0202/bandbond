ac8318800ca94c2268c678bd839e6196dc79cff4
yuki-0202 <stoicbazooka@gmail.com>
2022-03-19 12:54:34 +0900

    テストコード修正(room)

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,29 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  before do
     user = FactoryBot.create(:user)
-    @room = FactoryBot.build(:room)
-  end
+    let(:room) { FactoryBot.create(:room) }
 
   describe 'チャットルーム作成' do
     context '新規作成できる場合' do
-      it 'roomとuserが紐づいていれば保存できる' do
-        expect(@room).to be_valid
+      it 'bookingとuserが紐づいていれば保存できる' do
+        expect(room).to be_valid
       end
     end
 
-    # context '新規作成できない場合' do
-    #   it 'roomが紐づいていないと保存できない' do
-    #     @room.booking_id = nil
-    #     @room.valid?
-    #     expect(@room.errors.full_messages).to include('Room must exist')
-    #   end
-    #   it 'userが紐づいていないと保存できない' do
-    #     @room.user_id = nil
-    #     @room.valid?
-    #     expect(@room.errors.full_messages).to include('User must exist')
-    #   end
-    # end
+    context '新規作成できない場合' do
+      it 'bookingが紐づいていないと保存できない' do
+        room.booking_id = nil
+        expect(room).to be_invalid
+      end
+      it 'userが紐づいていないと保存できない' do
+        # room.user_id = nil
+        # expect(room).to be_invalid
+      end
+    end
   end
 end
