02ad37cb309d97cb480166cd7355c480ed11f7d0
yuki-0202 <stoicbazooka@gmail.com>
2022-03-28 21:13:39 +0900

    チャット管理機能のexample洗い出し

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Messages', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+RSpec.describe 'チャット管理機能', type: :system do
+  context 'チャット投稿テスト' do
+    it 'チャットの投稿に成功すると、再びルーム詳細へ戻り、投稿した内容が表示されている' do
+    end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    it '送る値が空の場合、チャットの投稿に失敗する' do
+    end
+
+    it 'ブッキング投稿が削除されると、関連するルームとチャットが全て削除される' do
+    end
+  end
 end
