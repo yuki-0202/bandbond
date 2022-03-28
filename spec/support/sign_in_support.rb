c99454683591d82f8271329cf3a1ff7a631f5375
yuki-0202 <stoicbazooka@gmail.com>
2022-03-28 11:39:55 +0900

    READMEの更新

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -11,8 +11,8 @@ module SignInSupport
   private
 
   def basic_pass(path)
-    username = 'admin'
-    password = '7777'
+    username = ENV['BASIC_AUTH_USER']
+    password = ENV['BASIC_AUTH_PASSWORD']
     visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
   end
 end
