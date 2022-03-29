1a32378e119e0681568ec92e3aff5df976ebb8d9
yuki-0202 <stoicbazooka@gmail.com>
2022-03-29 12:00:48 +0900

    ルーム管理機能のexample洗い出し

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Rooms', type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'ルーム管理機能', type: :system do
+  context 'ルーム自動作成テスト' do
+    it 'ルーム自動作成に成功し、ルーム詳細ページへ遷移する' do
+    end
+    it 'すでにルームが存在する場合、ルーム自動作成は実行されず、該当のルーム詳細ページへ遷移する' do
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ルーム自動削除テスト' do
+    it 'ルーム自動削除に成功し、ブッキング詳細ページへ遷移する' do
+    end
+    it 'ルーム内にチャットが存在する場合、ルーム自動削除は実行されず、ブッキング詳細ページへ遷移する' do
+    end
+  end
 end
